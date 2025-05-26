95217e4072ace07f1a53586451e436e89d9cc9fe
Tarek-Siddique-Nabil <nabilh420@gmail.com>
2025-05-27 01:57:36 +0600

    fix: reorder import statements in drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,6 @@
-import env from "@/validation/env";
 import { defineConfig } from "drizzle-kit";
 
+import env from "@/validation/env";
 
 export default defineConfig({
   schema: "./src/db/schema/index.ts",
